cddf10d8b6f10312dee2859d7666c1ca250adbb9
wodoame <bmwodoame@gmail.com>
2026-03-09 12:28:04 +0000

    feat(backend): add service request controller (#1)

--- backend/src/main/java/com/servicehub/repository/SlaPolicyRepository.java
+++ backend/src/main/java/com/servicehub/repository/SlaPolicyRepository.java
@@ -1,12 +1,10 @@
 package com.servicehub.repository;
 
 import com.servicehub.model.SlaPolicy;
-import com.servicehub.model.enums.RequestCategory;
 import com.servicehub.model.enums.RequestPriority;
-import java.util.Optional;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface SlaPolicyRepository extends JpaRepository<SlaPolicy, UUID> {
-    Optional<SlaPolicy> findByCategoryAndPriority(RequestCategory category, RequestPriority priority);
+public interface SlaPolicyRepository extends JpaRepository<SlaPolicy, Long> {
+    Optional<SlaPolicy> findByPriority(RequestPriority priority);
 }
